606f16e3b03454a881d3bdebb8855fbccda82606
Unknown <georgecranmermorgan@outlook.com>
2020-05-25 02:37:20 +0100

    Initial commit

---
+++ Discord.Bot/Bot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+
+namespace Discord.Bot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var config = new DiscordConfiguration
+            {
+
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+
+            //Makes sure the bot completes it's task before quitting
+            await Task.Delay(1);
+        }
+
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return null;
+        }
+    }
+}
---
+++ Discord.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Discord.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
